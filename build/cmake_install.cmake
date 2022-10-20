c154a335baf138053a82f31aac065f9a94a1d30b
OsipovDN <disorder_2@list.ru>
2022-10-20 23:12:50 +0300

    Добавлены изменения по оформлению кода

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: F:/C++/Kurs/OTUS/C++ Developer Basic/05_Модульность. Линковка/DZ_03/OTUS_Basic_DZ_03_v1
+# Install script for directory: F:/C++/Kurs/OTUS/C++ Developer Basic/05_Модульность. Линковка/DZ_03/OTUS_Basic_DZ_03_
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/C++/Kurs/OTUS/C++ Developer Basic/05_Модульность. Линковка/DZ_03/OTUS_Basic_DZ_03_v1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/C++/Kurs/OTUS/C++ Developer Basic/05_Модульность. Линковка/DZ_03/OTUS_Basic_DZ_03_/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
